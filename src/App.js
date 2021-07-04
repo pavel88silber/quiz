e71559fcee09ec6e0822bcc86f8939ac58265394
Pavel Silber <silberpav@gmail.com>
2021-07-04 20:34:29 +0300

    3. Подключение CSS-модулей

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./hoc/Layout/Layout"
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+  
+        <Layout>
+          <div style={ {border: '1px solid black', width: 400, background: 'pink'} }>
+            <h1>Layout works!</h1>
+          </div>
+        </Layout>
+  
+      </div>
+    )
+  }
+
 }
 
 export default App;
+
+
+
+
+
+
