8715d6a3a46bbbb702240e484940b3f41c83fe04
Pavel Silber <silberpav@gmail.com>
2021-07-05 20:11:54 +0300

    4. Создание главной страницы

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import Layout from "./hoc/Layout/Layout"
-import React, {Component} from "react";
+import React, {Component} from "react"
+import Quiz from '../src/container/Quiz/Quiz'
+
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
   
         <Layout>
-          <div style={ {border: '1px solid black', width: 400, background: 'pink'} }>
-            <h1>Layout works!</h1>
-          </div>
+
+          <Quiz />
+          
         </Layout>
   
       </div>
---
+++ src/container/Quiz/Quiz.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import classes  from './Quiz.css'
+
+class Quiz extends Component {
+    render() {
+        return (
+            <div className={classes.Quiz}>
+                <h1>Quiz</h1>
+            </div>
+        )
+    }
+}
+
+export default Quiz
